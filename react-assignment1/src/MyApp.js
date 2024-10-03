5414f8c6ae9ada7b72d5cc0c6519efe7a22d576f
pragtoppo <pragtoppo@gmail.com>
2024-10-03 00:43:06 -0700

    pdated tasks

--- react-assignment1/src/MyApp.js
+++ react-assignment1/src/MyApp.js
@@ -1,30 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from './Table';
-
-const characters = [
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    },
-];
+import Form from './Form';
 
 function MyApp() {
-    return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-      );
+   const [characters, setCharacters] = useState([ ]);
+
+   function removeOneCharacter(index) {
+      const updated = characters.filter((character, i) => {
+         return i !== index;
+      });
+      setCharacters(updated);
+   }
+
+   return (
+     <div className="container">
+       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+     </div>
+   );
 }
+
 export default MyApp;
