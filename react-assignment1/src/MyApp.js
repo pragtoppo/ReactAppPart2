8c08fcdfa1668206091d1fc8a0db29a99c67feb9
pragtoppo <pragtoppo@gmail.com>
2024-10-03 00:58:45 -0700

    Updated tasks

--- react-assignment1/src/MyApp.js
+++ react-assignment1/src/MyApp.js
@@ -3,19 +3,23 @@ import Table from './Table';
 import Form from './Form';
 
 function MyApp() {
-   const [characters, setCharacters] = useState([ ]);
+   const [characters, setCharacters] = useState([]);
 
    function removeOneCharacter(index) {
-      const updated = characters.filter((character, i) => {
-         return i !== index;
-      });
+      const updated = characters.filter((character, i) => i !== index);
       setCharacters(updated);
    }
 
+   function updateList(person) {
+      setCharacters([...characters, person]);
+   }
+
    return (
-     <div className="container">
-       <Table characterData={characters} removeCharacter={removeOneCharacter} />
-     </div>
+      <div className="container">
+         <Table characterData={characters} removeCharacter={removeOneCharacter} />
+         {/* Add the Form component to allow adding new characters */}
+         <Form handleSubmit={updateList} />
+      </div>
    );
 }
 
