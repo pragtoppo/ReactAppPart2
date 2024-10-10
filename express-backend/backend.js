3daa37d4861463d44533ba07e95ec56f5ae2eb50
pragtoppo <pragtoppo@@gmail.com>
2024-10-10 13:56:00 -0700

    Updated tasks

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -39,7 +39,21 @@ const users = {
        }
     ]
  }
+ app.get('/users', (req, res) => {
+   const name = req.query.name;
+   if (name != undefined){
+       let result = findUserByName(name);
+       result = {users_list: result};
+       res.send(result);
+   }
+   else{
+       res.send(users);
+   }
+});
 
+const findUserByName = (name) => { 
+   return users['users_list'].filter( (user) => user['name'] === name); 
+}
  app.post('/users', (req, res) => {
    const userToAdd = req.body;
    addUser(userToAdd);
